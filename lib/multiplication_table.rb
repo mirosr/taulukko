aa0fa2f8d6f6af8c8e7f28d8c43ae929bfcca530
Miroslav Rachev <miro@mirosr.net>
2015-02-01 19:10:41 +0200

    feat(table): add MultiplicationTable implementation

--- lib/multiplication_table.rb
+++ lib/multiplication_table.rb
@@ -1,4 +1,26 @@
 module MultiplicationTable
   def self.make(primes, decorator)
+    table = multiply_numbers(primes)
+
+    table.reduce('') do |memo, row|
+      memo += row.join(' ')
+      memo += "\n" unless row == table.last
+      memo
+    end
+  end
+
+  private
+
+  def self.multiply_numbers(numbers)
+    [].tap do |table|
+      numbers.each_with_index do |number, index|
+        table << ['-', numbers].flatten if index == 0
+        table << multiply_row(numbers, number)
+      end
+    end
+  end
+
+  def self.multiply_row(numbers, multiplier)
+    [multiplier] << numbers.map { |number| number * multiplier }
   end
 end
--- spec/multiplication_table_spec.rb
+++ spec/multiplication_table_spec.rb
@@ -1,7 +1,22 @@
 require_relative 'spec_helper'
 
 describe MultiplicationTable do
-  it 'responds to .make' do
-    MultiplicationTable.must_respond_to :make
+  it 'returns a multiplication table of the given prime numbers' do
+    output = MultiplicationTable.make(primes, nil)
+
+    output.must_equal table
+  end
+
+  private
+
+  def primes
+    [3, 5, 7]
+  end
+
+  def table
+'- 3 5 7
+3 9 15 21
+5 15 25 35
+7 21 35 49'
   end
 end
