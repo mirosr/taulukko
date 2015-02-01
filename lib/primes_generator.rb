1bca827d747d285c16623d1224966ba92faa61c0
Miroslav Rachev <miro@mirosr.net>
2015-02-01 20:24:41 +0200

    feat(generator): add PrimesGenerator implementation

--- lib/primes_generator.rb
+++ lib/primes_generator.rb
@@ -1,4 +1,21 @@
 module PrimesGenerator
   def self.generate(count)
+    [].tap do |primes|
+      number = 2
+      while primes.count < count
+        primes << number if prime?(number)
+        number += 1
+      end
+    end
+  end
+
+  private
+
+  def self.prime?(number)
+    return number == 2 if number % 2 == 0
+
+    (3..Math.sqrt(number)).step(2) do |x|
+      return false if number % x == 0
+    end
   end
 end
--- spec/primes_generator_spec.rb
+++ spec/primes_generator_spec.rb
@@ -1,7 +1,13 @@
 require_relative 'spec_helper'
 
 describe PrimesGenerator do
-  it 'responds to .generate' do
-    PrimesGenerator.must_respond_to :generate
+  it 'generates a list of the first n prime numbers' do
+    PrimesGenerator.generate(4).must_equal first_4_primes
+  end
+
+  private
+
+  def first_4_primes
+    [2, 3, 5, 7]
   end
 end
