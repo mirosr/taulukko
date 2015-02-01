122af13713c48d7e7ce2240db55f61798374f4cd
Miroslav Rachev <miro@mirosr.net>
2015-02-01 16:46:30 +0200

    test(app): change Application module's spec into an acceptance test

--- lib/application.rb
+++ lib/application.rb
@@ -1,4 +1,10 @@
 module Application
   def self.run
   end
+
+  private
+
+  def self.primes_count
+    10
+  end
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,7 +1,29 @@
 require_relative 'spec_helper'
 
 describe Application do
-  it 'reponds to .run' do
-    Application.must_respond_to :run
+  it 'prints out a multiplication table of the first n prime numbers' do
+    output, = capture_io do
+      Application.stub(:primes_count, 3) do
+        Application.run
+      end
+    end
+
+    output.must_equal formatted_table
+  end
+
+  private
+
+  def formatted_table
+    <<EOS
++----+----+----+-----+
+|    | 2  | 3  | 5   |
++----+----+----+-----+
+| 2  | 4  | 6  | 10  |
++----+----+----+-----+
+| 3  | 6  | 9  | 15  |
++----+----+----+-----+
+| 5  | 10 | 15 | 25  |
++----+----+----+-----+
+EOS
   end
 end
