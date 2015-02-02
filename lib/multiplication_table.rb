bac498bc712b9a96021e1ba251676ee6a4b83734
Miroslav Rachev <miro@mirosr.net>
2015-02-02 02:21:08 +0200

    refactor(table): remove #multiply_numbers from MultiplicationTable

--- lib/multiplication_table.rb
+++ lib/multiplication_table.rb
@@ -11,27 +11,23 @@ class MultiplicationTable
   def make
     return '' if @primes.empty?
 
-    table.reduce('') do |memo, row|
-      memo += @decorator.format(row, row == table.last)
+    multiplication_table.reduce('') do |memo, row|
+      memo += @decorator.format(row, row == multiplication_table.last)
     end
   end
 
   private
 
-  def table
-    @table ||= multiply_numbers
-  end
-
-  def multiply_numbers
-    [].tap do |table|
+  def multiplication_table
+    @multiplication_table ||= [].tap do |table|
       @primes.each_with_index do |number, index|
         table << ['-', @primes].flatten if index == 0
-        table << multiply_row(@primes, number)
+        table << multiplication_row(@primes, number)
       end
     end
   end
 
-  def multiply_row(numbers, multiplier)
+  def multiplication_row(numbers, multiplier)
     [multiplier] + numbers.map { |number| number * multiplier }
   end
 end
